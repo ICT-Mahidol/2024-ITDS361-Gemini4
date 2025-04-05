e2c1af5c8deccc3810da3d700b7194cac08c8452
chocody <suntipap.cho@student.mahidol.edu>
2025-04-05 21:36:33 +0700

    update folder

--- implementations/gemini4-backend/src/main/java/com/gemini4/gemini4_backend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gemini4.gemini4_backend.repository;
-
-import com.gemini4.gemini4_backend.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
